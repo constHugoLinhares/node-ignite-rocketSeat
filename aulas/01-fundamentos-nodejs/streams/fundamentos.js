fa51ef6b9ece609186a8986aa32571a9a3b794f4
Hugo Linhares <hugolinhares92@gmail.com>
2023-11-06 14:47:25 -0300

    Streams de escrita e transformação

--- aulas/01-fundamentos-nodejs/streams/fundamentos.js
+++ aulas/01-fundamentos-nodejs/streams/fundamentos.js
@@ -17,7 +17,7 @@
 // process.stdin
 //     .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class oneToHundredStream extends Readable {
     index = 1
@@ -36,6 +36,22 @@ class oneToHundredStream extends Readable {
         }, 100)
     }
 }
+class inverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class multiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
 
 new oneToHundredStream()
-    .pipe(process.stdout)
+    .pipe(new inverseNumberStream())
+    .pipe(new multiplyByTenStream())
