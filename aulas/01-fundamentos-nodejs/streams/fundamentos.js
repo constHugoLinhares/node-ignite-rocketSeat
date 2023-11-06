fbc005b6559686d138e111ba5f9caf0e106d2a8f
Hugo Linhares <hugolinhares92@gmail.com>
2023-11-06 14:24:48 -0300

    Criando stream de leitura

--- aulas/01-fundamentos-nodejs/streams/fundamentos.js
+++ aulas/01-fundamentos-nodejs/streams/fundamentos.js
@@ -10,3 +10,32 @@
  * 
  * Readable Streams (Leitura aos poucos de uma determinada requisição) / Writable Streams
  */
+
+
+//  Streams -> 
+
+// process.stdin
+//     .pipe(process.stdout)
+
+import { Readable } from 'node:stream'
+
+class oneToHundredStream extends Readable {
+    index = 1
+
+    _read() {
+        const i = this.index++
+        
+        setTimeout(() => {
+            if(i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i));
+    
+                this.push(buf)
+            }
+        }, 100)
+    }
+}
+
+new oneToHundredStream()
+    .pipe(process.stdout)
