67304e21a54b00f8f37d557320e5c5ac82a56138
Hugo Linhares <hugolinhares92@gmail.com>
2023-11-08 18:55:56 -0300

    Rotas com parâmetros (RegEx)

--- aulas/01-fundamentos-nodejs/src/utils/build-route-path.js
+++ aulas/01-fundamentos-nodejs/src/utils/build-route-path.js
@@ -1,7 +1,9 @@
 // /users/:id
 export function buildRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-    console.log(Array.from(path.matchAll(routeParametersRegex)))
-    return new RegExp()
+    const pathRegex = new RegExp(`^${pathWithParams}`)
+
+    return pathRegex
 }
