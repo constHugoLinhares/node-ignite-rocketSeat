8bd547513236e788f4ef3ae89073000b15501af4
Hugo Linhares <hugolinhares92@gmail.com>
2023-11-26 18:40:06 -0300

    fix2

--- aulas/02-api-rest-nodejs/src/database.ts
+++ aulas/02-api-rest-nodejs/src/database.ts
@@ -1,13 +1,8 @@
 import { knex as setupKnex, Knex } from 'knex'
 import 'dotenv/config'
 
-<<<<<<< HEAD
 if(!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL env Not Found!')
-=======
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE env Not Found!')
->>>>>>> 33597ca093ba6f135c8df2e931db58a5cadb0035
 }
 
 export const config: Knex.Config = {
