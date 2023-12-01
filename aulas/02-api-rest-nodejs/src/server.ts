c595bf85c8761690c9d90679c5d209861d709138
Hugo Linhares <hugolinhares92@gmail.com>
2023-12-01 10:49:47 -0300

    Criação das transações

--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -1,24 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-// GET, POST, PUT, PATCH, DELETE
-
-app.get('/hello', async () => {
-  // const transactions = await knex('transactions')
-  // .where('amount', 1000)
-  // .select('*')
-
-  const transactions = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Transação de teste',
-    amount: 1000
-  }).returning('*')
-
-  return transactions
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
