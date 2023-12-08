b31b7d94d7e1e714955b97debc54c32b9b866941
Hugo Linhares <hugolinhares92@gmail.com>
2023-12-08 15:12:32 -0300

    Categorizando os testes

---
+++ aulas/02-api-rest-nodejs/test/transactions.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test, beforeAll, afterAll, describe } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+
+describe('Transaction Routes', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    test('User can create a new transaction', async () => {
+        await request(app.server)
+            .post('/transactions')
+            .send(
+                {
+                    title: 'New transaction',
+                    amount: 5000,
+                    type: 'credit',
+                }
+            )
+            .expect(201)
+    })
+})
