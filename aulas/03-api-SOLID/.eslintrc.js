8cc73e421762804106bcc55ebf0af64053b64216
Hugo Linhares <hugolinhares92@gmail.com>
2024-01-09 14:05:34 -0300

    Finalization of project structure

---
+++ aulas/03-api-SOLID/.eslintrc.js
@@ -0,0 +1,35 @@
+
+module.exports = {
+	'env': {
+		'es2021': true,
+		'node': true,
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+  
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'sourceType': 'module',
+		'ecmaVersion': 'latest',
+	},
+	'plugins': ['@typescript-eslint'],
+	'rules': {
+		'indent': ['error','tab'],
+		'quotes': ['error','single'],
+		'semi': ['error','always'],
+		'no-undef': 'off',
+		'arrow-spacing': ['error', { 'before': true, 'after': true }],
+		'promise/always-return': 'off',
+		'promise/catch-or-return': 'off',
+		'promise/async-await-error-handling': 'off',
+		'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+		'keyword-spacing': ['error', { 'before': true, 'after': true }],
+		'space-before-blocks': ['error', 'always'],
+		'object-curly-spacing': ['error', 'always'],
+		'object-curly-newline': ['error', { 'multiline': true, 'consistent': true }],
+		'object-property-newline': ['error', { 'allowAllPropertiesOnSameLine': true }],
+		'comma-dangle': ['error', 'always-multiline'],
+	},
+};
--- aulas/03-api-SOLID/src/server.ts
+++ aulas/03-api-SOLID/src/server.ts
@@ -1,9 +1,9 @@
-import { env } from './ENV'
-import { app } from './app'
+import { env } from './ENV';
+import { app } from './app';
 
 app.listen({
-    host: '0.0.0.0',
-    port: env.PORT,
+	host: '0.0.0.0',
+	port: env.PORT,
 }).then(() => {
-    console.log('🚀 HTTP Server Running!')
-})
+	console.log('🚀 HTTP Server Running!');
+});
