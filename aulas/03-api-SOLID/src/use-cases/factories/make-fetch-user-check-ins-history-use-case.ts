19adac887fb6cf45db7eb7fa41e016c90acdab2a
Hugo Linhares <hugolinhares92@gmail.com>
2024-05-08 07:07:04 -0300

    Factories dos casos de uso

---
+++ aulas/03-api-SOLID/src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository';
+import { FetchUserCheckInsHistoryUseCase } from '../fetch-user-member-check-ins-history';
+
+export const makeFetchUserCheckInsHistoryUseCase = () => {
+	const checkInsRepository = new PrismaCheckInsRepository();
+	const fetchUserCheckInsHistoryUseCase = new FetchUserCheckInsHistoryUseCase(checkInsRepository);
+
+	return fetchUserCheckInsHistoryUseCase;
+};
