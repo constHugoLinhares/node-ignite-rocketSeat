f5064cf1f3b9c67b4d6f8189d8bac7bb7f1dbcb4
Hugo Linhares <hugolinhares92@gmail.com>
2024-05-13 07:00:18 -0300

    test environment do prisma

--- aulas/03-api-SOLID/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ aulas/03-api-SOLID/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,43 @@
+import 'dotenv/config';
+
+import { randomUUID } from 'crypto';
+import { execSync } from 'child_process';
 import { Environment } from 'vitest';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default <Environment>{
 	name: 'prisma',
 	transformMode: 'ssr',
 	async setup() {
-		console.log('Executed');
+		const schema = randomUUID();
+		const databaseURL = generateDatabaseURL(schema);
+
+		process.env.DATABASE_URL = databaseURL;
+
+		execSync('npx prisma migrate deploy');
 
 		return {
-			teardown() { 
-				console.log('teardown');
+			async teardown() { 
+				await prisma.$executeRawUnsafe(
+					`DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+				);
+
+				await prisma.$disconnect();
 			},
 		};
 	},
 };
+
+function generateDatabaseURL(schema:string) {
+	if (!process.env.DATABASE_URL) {
+		throw new Error('DATABASE_URL is not defined');
+	}
+
+	const url = new URL(process.env.DATABASE_URL);
+
+	url.searchParams.set('schema', schema);
+
+	return url.toString();
+}
