5fa72748eae5407a303e64141840b99001f67383
Hugo Linhares <hugolinhares92@gmail.com>
2024-05-14 06:39:22 -0300

    Teste  E2E da autenticação

--- aulas/03-api-SOLID/src/http/controllers/register.spec.ts
+++ aulas/03-api-SOLID/src/http/controllers/register.spec.ts
@@ -1,4 +1,25 @@
-import { test } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
 
+describe('Register (e2e)', () => {
+	beforeAll(async () => {
+		await app.ready();
+	});
 
-test('ok', () => {});
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it('Should be able to register', async () => {
+		const response = await request(app.server)
+			.post('/users')
+			.send({
+				name: 'Hugo',
+				email: 'rockerhugo@gmail.com',
+				password: '123456',
+			});
+
+		expect(response.statusCode).toEqual(201);
+	});
+});
