3c1a1a25839a2a42f59513cb893c5e5592a5c114
Hugo Linhares <hugolinhares92@gmail.com>
2024-05-15 06:18:43 -0300

    controller da criação de academias

---
+++ aulas/03-api-SOLID/src/http/controllers/gyms/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from 'fastify';
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt';
+
+
+
+export async function usersRoutes(app: FastifyInstance) {
+	app.addHook('onRequest', verifyJWT);
+}
